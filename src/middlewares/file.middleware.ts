65782b8df20c90690aeb4ecd3bd08e6422d1edaa
NewtTheWolf <59208097+NewtTheWolf@users.noreply.github.com>
2022-02-18 18:54:10 +0100

    updated folder

--- src/middlewares/file.middleware.ts
+++ src/middlewares/file.middleware.ts
@@ -5,18 +5,14 @@ export async function fileMiddleware(context: Context) {
   // FAVICON IS SOO COOL SO IT GETS ITS OWN SPECIAL PATH
   if (/^\/favicon.(png|ico)$/.test(context.request.url.pathname)) {
     return await send(context, "/favicon.png", {
-      root: "./src/frontend/build",
+      root: "./src/frontend/public",
     });
   }
 
   // NON ETA FILES SHOULD NOT BE RENDERED
-  if (
-    /\.(css|js|png|jpg|ico|woff2|woff|ttf|wasm|svg|map)$/.test(
-      context.request.url.pathname
-    )
-  ) {
+  if (/\.(css|js|png|jpg|ico|woff2|woff|ttf|wasm|svg|map)$/.test(context.request.url.pathname)) {
     return await send(context, context.request.url.pathname, {
-      root: "./src/frontend/build",
+      root: "./src/frontend/public",
     });
   }
 }
